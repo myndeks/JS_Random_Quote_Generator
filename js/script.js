cfe3bc45c7900f0346005c3f3c319a17fde4d6e9
Mindaugas D <myndeks@gmail.com>
2019-12-30 19:23:12 +0100

    moving foward

--- js/script.js
+++ js/script.js
@@ -10,19 +10,65 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
-
+let quotes = [
+  {
+     quote : " “I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.” ",
+     source : 'Marilyn Monroe',
+     citation :"",
+     year : "2005"
+  },
+  {
+    quote : " “You only live once, but if you do it right, once is enough.” ",
+    source : 'Mae West',
+    citation :"",
+    year : ""
+  },
+  {
+    quote : " “In three words I can sum up everything I've learned about life: it goes on.”  ",
+    source : 'Robert Frost ',
+    citation :"From some of the movies",
+    year : ""
+  },
+  {
+    quote : " “To live is the rarest thing in the world. Most people exist, that is all.”  ",
+    source : 'Oscar Wilde',
+    citation :"",
+    year : ""
+  },
+  {
+    quote : " “Good friends, good books, and a sleepy conscience: this is the ideal life.” ",
+    source : 'Mark Twain ',
+    citation :"",
+    year : ""
+  }
+]
 
 
 /***
  * `getRandomQuote` function
 ***/
-
+function getRandomQuote() {
+  let randomNumber = Math.floor(Math.random() * quotes.length ) + 1;
+  return randomNumber;
+}
+console.log(getRandomQuote());
 
 
 /***
  * `printQuote` function
 ***/
+function printQuote () {
+  let randomQuoteObject =  getRandomQuote();
+  let storeHtml = '';
 
+
+{/* <p class="quote"> [quote here] </p>
+<p class="source"> [source here]
+  <span class="citation"> [citation here] </span>
+  <span class="year"> [year here] </span>
+</p> */}
+
+}
 
 
 /***
@@ -30,4 +76,4 @@ project 1 - A Random Quote Generator
  * DO NOT CHANGE THE CODE BELOW!!
 ***/
 
-document.getElementById('load-quote').addEventListener("click", printQuote, false);
+// document.getElementById('load-quote').addEventListener("click", printQuote, false);
